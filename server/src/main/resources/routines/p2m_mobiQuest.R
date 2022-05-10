f67e67a2c3360818d8d4283cf549f7ef15eb0ad7
DOUET <aurelie.douet@gmail.com>
2022-05-10 16:38:04 +0200

    upd p2m

--- server/src/main/resources/routines/p2m_mobiQuest.R
+++ server/src/main/resources/routines/p2m_mobiQuest.R
@@ -12,11 +12,11 @@ cheminOut <- "../../../../../../../data_web/data/"
 # choix des params
 
 ## Ville
-nomEnq = "IDF"
+nomEnq = "SAO PAULO"
 
 ## requête : changement du périmètre observé, exemple :
 perim <- c(1, 2, 3)
-perim <- c()
+perim <- c(7)
 ## requête : sous-population avec n filtre, exemple :
 # subpop <- list("SEX" = "2", "EDUC" = "4", "STRM" = "1")
 # subpop <- list("SEX" = "2", "KAGE" = c("1","2"))
@@ -24,6 +24,22 @@ subpop <- list("OCC" = c(1, 2, 3, 4, 5) , "CSPMEN" =  c(1, 2, 3, 4, 5)  , "QPV" 
                "CSP" = c(1, 2, 3, 4, 5) , "SEX" =  c(1, 2)  , "ZONAGE" =  c(1, 2, 3) , 
                "KAGE" =  c(1, 2, 3, 4), "EDUC" =  c(1, 2, 3, 4), "EDUCMEN" =  c(1, 2, 3, 4), 
                "STRM" =  c(1, 2, 3, 4), "REV" = c(), "DEP" = c(1, 2, 3, 4, 5))
+subpop <- list("OCC" = c(1, 2, 3, 4, 5) , "CSPMEN" =  c(1, 2, 3, 4, 5)  , "QPV" = c(1, 2) , 
+               "CSP" = c(1, 2, 3, 4, 5) , "SEX" =  c(1, 2)  , "ZONAGE" =  c(1, 2, 3) , 
+               "KAGE" =  c(1), "EDUC" =  c(1, 2, 3, 4), "EDUCMEN" =  c(1, 2, 3, 4), 
+               "STRM" =  c(1, 2, 3, 4))
+subpop <- c()
+subpop <- list( "INFORMAL" =  c() , "REV" =  c(1, 2, 3, 4, 5
+) , "CSO" =  c(1, 2, 3, 4
+) , "LOG" =  c(1, 3
+) , "SEX" =  c(1
+) , "EDUCMEN" =  c(1, 2, 3, 4
+) , "ZONAGE" =  c(1, 2, 3, 4
+) , "EDUC" =  c(1, 4
+) , "STRM" =  c(1, 2, 3, 4, 5
+) , "OCC" =  c(2, 3, 4, 5
+) , "KAGE" =  c(1, 2, 3, 4
+) )
 
 # load p2m functions
 suppressWarnings(source("p2m_fct_mobiQuest.R"))
